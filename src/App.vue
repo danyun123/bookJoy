595768180672d5f3ddea2b8ba1049860da360efc
zxk <1350620948@qq.com>
2023-04-30 13:29:03 +0800

    完成项目基础配置

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app">
+		我是嘻嘻嘻嘻嘻嘻嘻嘻寻
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped lang="sass">
+.app
+	font-size: 1rem
+</style>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import "@/assets/css/entire.scss"
+
+createApp(App).mount("#app");
